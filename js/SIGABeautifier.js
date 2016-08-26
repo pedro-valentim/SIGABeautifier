67735c2b126f99de6d2444b25852de1fa29aaf1c
Pedro Valentim <pedrovalentim.leite@hotmail.com>
2016-08-26 14:55:28 -0300

    - Troca métodos de replace pelo template engine Nunjucks! \o/ - Abre seção do menu que o item está sendo visualizado - Seleciona item do menu que está sendo visualizado

--- js/SIGABeautifier.js
+++ js/SIGABeautifier.js
@@ -1,96 +1,109 @@
 var sigabeautifier = {
-	user: {}
+	user: {},
+	sections: []
 };
 $(function() {
 
 	// send message to background script
     chrome.runtime.sendMessage({ "newIconPath" : '../img/icon-active.png' });
 
-	$("#TABLE100_MPAGE").attr('cellspacing', 25);
+    sigabeautifier.user.autenticado = $("#span_MPW0039vPRO_PESSOALNOME") != null;
 
-	sigabeautifier.user.nome = $("#span_MPW0039vPRO_PESSOALNOME").text().trim();
-	if (sigabeautifier.user.nome!='') {
-		var nome_split = sigabeautifier.user.nome.toLowerCase().split(" ");
-		sigabeautifier.user.nomecurto = nome_split[0].charAt(0).toUpperCase() + nome_split[0].slice(1) + " " + nome_split[1].charAt(0).toUpperCase() + nome_split[1].slice(1);
-	}
-	sigabeautifier.user.avatar = $("#MPW0039FOTO > img").attr('src');
-	sigabeautifier.user.ra = $("#span_MPW0039vACD_ALUNOCURSOREGISTROACADEMICOCURSO").text().trim();
-	sigabeautifier.user.mediacurso = $("#span_MPW0039vACD_ALUNOCURSOINDICEPR").text().trim();
-	sigabeautifier.user.progressao = $("#span_MPW0039vACD_ALUNOCURSOINDICEPP").text().trim();
-	sigabeautifier.user.maiormediacurso = $("#span_MPW0039vMAX_ACD_ALUNOCURSOINDICEPR").text().trim();
-	sigabeautifier.user.curso = $("#span_vACD_CURSONOME_MPAGE").text().trim();
-	sigabeautifier.user.periodo = $("#span_vACD_PERIODODESCRICAO_MPAGE").text().trim();
+    nunjucks.configure(chrome.extension.getURL("html/templates"), { autoescape: true });
 
-	// Atualiza painel de info do user
-	url = chrome.extension.getURL("html/templates/user-info-card.html");
-	$.get(url, function(data, textStatus, jqXHR){
-		var template = object_in_template(sigabeautifier.user, data);
-		$("#gxHTMLWrpMPW0039").html(template);
-	}, "html");
+	// Se algo der errado aqui mostrar body de qualquer forma (finally)
+    try {
 
-	// Atualiza menu lateral
-	url = chrome.extension.getURL("html/templates/sidemenu.html");
-	$.get(url, function(sidemenu_html, textStatus, jqXHR){
-		var sidemenu = $(sidemenu_html);
-		var sidemenu_sections = [];
-		url = chrome.extension.getURL("html/templates/sidemenu_section.html");
-		$.get(url, function(sidemenu_section_html, textStatus, jqXHR){
-			var id_sufix = 1;
+    	$("#TABLE100_MPAGE").attr('cellspacing', 25);
+
+    	if (sigabeautifier.user.autenticado) {
+
+    		sigabeautifier.user.nome = $("#span_MPW0039vPRO_PESSOALNOME").text().trim();
+
+			nome_split = sigabeautifier.user.nome.toLowerCase().split(" ");
+
+			sigabeautifier.user.nomecurto = nome_split[0].charAt(0).toUpperCase() + nome_split[0].slice(1) + " " + nome_split[1].charAt(0).toUpperCase() + nome_split[1].slice(1);
+			sigabeautifier.user.avatar = $("#MPW0039FOTO > img").attr('src');
+			sigabeautifier.user.ra = $("#span_MPW0039vACD_ALUNOCURSOREGISTROACADEMICOCURSO").text().trim();
+			sigabeautifier.user.mediacurso = $("#span_MPW0039vACD_ALUNOCURSOINDICEPR").text().trim();
+			sigabeautifier.user.progressao = $("#span_MPW0039vACD_ALUNOCURSOINDICEPP").text().trim();
+			sigabeautifier.user.maiormediacurso = $("#span_MPW0039vMAX_ACD_ALUNOCURSOINDICEPR").text().trim();
+			sigabeautifier.user.curso = $("#span_vACD_CURSONOME_MPAGE").text().trim();
+			sigabeautifier.user.periodo = $("#span_vACD_PERIODODESCRICAO_MPAGE").text().trim();
+
+			// Atualiza info do user (acima do menu lateral)
+			$("#gxHTMLWrpMPW0039").html( nunjucks.render('user-info-card.html', sigabeautifier.user) );
+
+			var id_prefix = "MenuLateralSecao_";
+
 			// Para cada seção do menu lateral...
 			$("#MPTREEVIEW1Container #ygtvc1 > .ygtvitem").each(function(i, ygtvitem) {
 				
 				var section = {};
-				section.secao_nome = $("> table .BasicNodeTextDecoration .NodeTextDecoration", $(this)).text();
-				section.secao_id = "MenuLateralSecao_" + $("> table .BasicNodeTextDecoration .NodeTextDecoration", $(this)).text() + id_sufix;
-				section.active_class = "";
-				if (section.secao_nome=='Consultas') section.active_class = "active";
 
-				var sidemenu_section = $(object_in_template(section, sidemenu_section_html));
-				sidemenu_section.click(function(){
-					$(this).toggleClass('active');
-				});
-				
+				section.nome = $("> table .BasicNodeTextDecoration .NodeTextDecoration", $(this)).text();
+				section.id = id_prefix + section.nome;
+				section.css_class = "";
+				section.fa_icon = "circle";
+				section.items = [];
+
 				// Para cada item na seção no menu
 				$(".ygtvchildren .ygtvitem", $(this)).each(function(){
-					var item_nome = $("> table .BasicNodeTextDecoration .NodeTextDecoration", $(this)).text();
 					var link = $("> table .BasicNodeTextDecoration", $(this));
 
-					var link_element = $("<a />").text(item_nome)
-											.css({
-												width: "100%",
-												display: "inline-block"
-											})
-											.attr("href", link.attr("href"))
-											.attr("target", link.attr("target"));
+					section.items.push({
+						href: link.attr("href"),
+						target: link.attr("target"),
+						nome: $(".NodeTextDecoration", link).text()
+					});
 
-					$("ul", sidemenu_section).append(
-						$("<li />")
-							.html( link_element )
-					);
 				});
 
-				$("#menu-content", sidemenu).append( sidemenu_section );
-				id_sufix = id_sufix + 1;
+				sigabeautifier.sections.push( section );
+
 			});
-			$("#MPTREEVIEW1Container").html(sidemenu);
+
+			// Filtra as seções pela url do link do seus items e a url da página
+			// e.g.: página: /aluno/horario.aspx, active_section = Consultas
+			var active_section = sigabeautifier.sections.filter(function(_section) {
+				var bool = false;
+				$.each(_section.items, function(i, _section_item) {
+					if ("/aluno/" + _section_item.href == window.location.pathname) {
+						_section_item.css_class = "highlight";
+						bool = true;
+					}
+				});
+				return bool;
+			})[0];
+
+			active_section.css_class = "active";
+
+			var sidemenu = $(nunjucks.render('sidemenu.html', sigabeautifier));
+
+			$("#TABLE2_MPAGE").before( sidemenu );
+			$("#TABLE2_MPAGE").remove();
+
+		}
+
+		// Ao clicar na seção mostra/esconde itens da seção
+		$(document).on("click", "#menu-content > li", function(event){
+			$(this).toggleClass("active");
 		});
-	}, "html");
 
-	// Remove borda de table wrapper do menu lateral
-	$("#TABLE2_MPAGE").css('border', 0);
+		// Impede que o click no item da seção do menu chame o click na seção
+		$(document).on("click", "#menu-content .sub-menu > li", function(event){
+			event.stopPropagation();
+		});
 
-	$("body").fadeIn("slow");
+		// Remove borda de table wrapper do menu lateral
+		$("#TABLE2_MPAGE").css('border', 0);
+	}
+	catch(err) {
+		window.console.error(err);
+	} 
+	finally {
+	    $("body").fadeIn("slow");
+	}
+
 
 });
-
-function object_in_template(object, template_html) {
-	var template = template_html;
-	for (attribute in object) {
-		template = object_attr_in_template(attribute, object[attribute], template);
-	}
-	return template;
-}
-
-function object_attr_in_template(object_attr_name, object_attr_value, template_html) {
-	return template_html.replace(new RegExp("{{[\s+]?" + object_attr_name + "[\s+]?}}", "g"), object_attr_value);
-}
