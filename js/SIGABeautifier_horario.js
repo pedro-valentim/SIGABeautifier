d06942cff6d7c40224e4991edce5d7a43e1ae369
Pedro Valentim <pedrovalentim.leite@hotmail.com>
2016-08-30 20:43:18 -0300

    Começando a utilizar nunjucks pra gerar HTML da página

--- js/SIGABeautifier_horario.js
+++ js/SIGABeautifier_horario.js
@@ -2,96 +2,113 @@ $(function(){
 
 	// Adiciona botão de impressão
 	$("<button onclick='javascript:print();' />")
-		.text('Imprimir horários')
-		.addClass('sigabeautifier-btn-print sigabeautifier-btn sigabeautifier-btn-primary sigabeautifier-pull-right no-print')
-		.addClass('sigabeautifier-absolute-right')
+		.text("Imprimir horários")
+		.addClass("sigabeautifier-btn-print sigabeautifier-btn sigabeautifier-btn-primary sigabeautifier-pull-right no-print")
+		.addClass("sigabeautifier-absolute-right")
 		.insertBefore( "#TABLE2" );
 
-	$("#TABELAINTERNA_MPAGE td:eq(0)").addClass('no-print');
+	$("#TABELAINTERNA_MPAGE td:eq(0)").addClass("no-print");
 
-	$('#TEXTBLOCK11 > input').remove();
+	$("#TEXTBLOCK11 > input").remove();
+
+	sigabeautifier.disciplinas = [];
 
 	// Para cada matéria na tabela de matérias...
-	$('#Grid1ContainerDiv #Grid1ContainerTbl tr[class^=GridClear]').each(function(i, el){
-		var disciplina = $('td:eq(1)', $(this));
-		var parts = disciplina.text().split('<br>');
-		var horas_aula_label = $('<span />').addClass('sigabeautifier-label sigabeautifier-label-primary').text(parts[1]);
-		disciplina.html(horas_aula_label);
-		disciplina.append(parts[0]);
+	$("#Grid1ContainerDiv #Grid1ContainerTbl tr[class^=GridClear]").each(function(i, el){
+		var nome_horasaula = $("td:eq(1)", $(this)).text().split("<br>");
+		sigabeautifier.disciplinas.push({
+			sigla: $("td:eq(0)", $(this)).text(),
+			nome: nome_horasaula[0],
+			horas_aula: nome_horasaula[1],
+			turma: $("td:eq(2)", $(this)).text(),
+			professor: $("td:eq(3)", $(this)).text()
+		});
 	});
 
+
+	var dias_semana = [
+		'Segunda-feira',
+		'Terça-feira',
+		'Quarta-feira',
+		'Quinta-feira',
+		'Sexta-feira',
+		'Sábado'
+	];
+
+	sigabeautifier.dias = [];
+
+	var i = 0;
+
 	// Para cada dia da semana na tabela de horarios/dias...
-	$('#TABLE3 table.GridClear').each(function(i, horario_dia) {
-		var horario_aulas = [];
+	$("#TABLE3 table.GridClear").each(function(i, horario_dia) {
+		var dia = {
+			nome: dias_semana[i],
+			horarios: []
+		};
 
-		$('tr:eq(0):not([class^=GridClear])', horario_dia).each(function(){
-			$('th.GridClearTitle:eq(0)', this).remove();
-		});
+		// Para cada aula/horário do dia
+		$("tr[class^=GridClear]", horario_dia).each(function(i, horario_aula){
+			var _horario_bruto = $("td:eq(1)", horario_aula).text().split("-");
+			var _horario_inicio = _horario_bruto[0];
+			var _horario_final = _horario_bruto[1];
+			var _sigla = $("td:eq(2)", horario_aula).text();
+			var _disciplina = sigabeautifier.disciplinas.find(function(element, index, array) {
+				return element.sigla == _sigla;
+			});
 
-		$('tr[class^=GridClear]', horario_dia).each(function(i, horario_aula){
-			var horario = $('td:eq(1)', horario_aula).text();
-			var sigla = $('td:eq(2)', horario_aula).text();
-			var turma = $('td:eq(3)', horario_aula).text();
-			var css_class = $(horario_aula).attr('class');
-			horario_aulas.push({
-				'horario': horario,
-				'sigla': sigla,
-				'turma': turma
+			dia.horarios.push({
+				inicio: new Date().setHours(_horario_inicio.split(":")[0], _horario_inicio.split(":")[1]),
+				inicio_raw: _horario_bruto[0],
+				final_raw: _horario_bruto[1],
+				disciplina: _disciplina,
+				turma: $("td:eq(3)", horario_aula).text()
 			});
 		});
 
-		// Remove todas os horários do dia
-		$('tr[class^=GridClear]', horario_dia).remove();
-
-		// Ordena o array de horários do dia pelo horário da aula
-		horario_aulas.sort(function(a, b){
-			var a = a.horario.split('-')[0];
-			var b = b.horario.split('-')[0];
-		    var keyA = new Date().setHours(a.split(':')[0], a.split(':')[1]),
-		        keyB = new Date().setHours(b.split(':')[0], b.split(':')[1]);
-		    if(keyA < keyB) return -1;
-		    if(keyA > keyB) return 1;
+		// Ordena os todos os horários do dia pelo horário de cada aula
+		dia.horarios.sort(function(current, next){
+			if(current.inicial < next.inicial) return -1;
+		    if(current.inicial > next.inicial) return 1;
 		    return 0;
 		});
 
-		// Readiciona horários do dia
-		$.each(horario_aulas, function(i, horario) {
-			var tr = $('<tr />').addClass('GridClear');
-			tr.append($('<td />').text(horario.horario));
-			tr.append($('<td />').addClass('sigabeautifier-horario').text(horario.sigla));
-			tr.append($('<td />').text(horario.turma));
-			$(horario_dia).append(tr);
-		});
+		sigabeautifier.dias.push(dia);
+
+		i++;
 	});
 
+	$("#TABLE3").remove();
 
-	$('#Grid1ContainerDiv #Grid1ContainerTbl tr[class^=GridClear]').click(function(){
+	$("#Grid1ContainerDiv").html(nunjucks.render("disciplinas_list.html", sigabeautifier));
+	$("#TABLE2").after(nunjucks.render("horarios_semana_list.html", sigabeautifier));
 
-		var not_reselecting = $(this).hasClass('sigabeautifier-active') == false;
+	$("#Grid1ContainerDiv #Grid1ContainerTbl tr[class^=GridClear]").click(function(){
+
+		var not_reselecting = $(this).hasClass("sigabeautifier-active") == false;
 
 		// Remove classe que destaca linhas da tabela de matérias
-		$('#Grid1ContainerDiv #Grid1ContainerTbl tr.sigabeautifier-active').removeClass('sigabeautifier-active');
+		$("#Grid1ContainerDiv #Grid1ContainerTbl tr.sigabeautifier-active").removeClass("sigabeautifier-active");
 
 		// Remove classe que inverte cor de fundo do label
-		$('#Grid1ContainerDiv #Grid1ContainerTbl span.sigabeautifier-label-reverse').removeClass('sigabeautifier-label-reverse');
+		$("#Grid1ContainerDiv #Grid1ContainerTbl span.sigabeautifier-label-reverse").removeClass("sigabeautifier-label-reverse");
 
 		// Remove classe que destaca linhas das tabelas de horário
-		$('#TABLE3 tr.GridClear').removeClass('sigabeautifier-active');
+		$("#TABLE3 tr.GridClear").removeClass("sigabeautifier-active");
 
 		// Se o item já não estava selecionado
 		if (not_reselecting) {
 
 			// Adiciona classe que inverte cor de fundo do label da linha selecionada
-			$('span.sigabeautifier-label', $(this)).addClass('sigabeautifier-label-reverse');
+			$("span.sigabeautifier-label", $(this)).addClass("sigabeautifier-label-reverse");
 			
 			// Adiciona classe que destaca linha selecionada
-			$(this).addClass('sigabeautifier-active');
+			$(this).addClass("sigabeautifier-active");
 
-			var sigla = $('td:eq(0)', $(this)).text();
+			var sigla = $("td:eq(0)", $(this)).text();
 
-			$('#TABLE3 tr.GridClear .sigabeautifier-horario').each(function(i, element) {
+			$("#TABLE3 tr.GridClear .sigabeautifier-horario").each(function(i, element) {
 				// Destaca linhas das tabelas de horário que forem da mesma disciplina clicada
-				if ($(element).text() == sigla) $(element).parents('tr.GridClear').addClass('sigabeautifier-active');
+				if ($(element).text() == sigla) $(element).parents("tr.GridClear").addClass("sigabeautifier-active");
 			});
 
 		}
