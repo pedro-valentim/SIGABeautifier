896e3801303a446b9ccf4cff49114d69e7328c2c
Pedro Valentim <pedro@znc.com.br>
2017-01-26 02:06:43 -0200

    login funcionando com sugestão do @zokis criar um form falso que quando o botão fosse clicado colocasse os valores dos campos falsos nos verdadeiros e chamasse o click do botão original + ajustes de aparencia

--- js/SIGABeautifier_login.js
+++ js/SIGABeautifier_login.js
@@ -21,52 +21,89 @@ $(function(){
 		return this[0].outerHTML.split(this.html())[0];
 	};
 
-	_html = {
-		user_field: $('#vSIS_USUARIOID')[0].outerHTML,
-		password_field: $('#vSIS_USUARIOSENHA')[0].outerHTML,
-		gxstate_field: $('input[type="hidden"][name="GXState"]')[0].outerHTML,
-		gxErrorViewer: $('#gxErrorViewer')[0].outerHTML,
-		submit_btn: $('input[type="button"][name="BTCONFIRMA"]')[0].outerHTML,
-		help_text: $('#TEXTBLOCK7')[0].outerHTML,
-		message: $('#TEXTBLOCK3 > div:first')[0].outerHTML,
-	};
-
-	$('#gxErrorViewer').remove();
-	$('input[type="hidden"][name="GXState"]').remove();
-
-	_html.user_field = $(_html.user_field).attr('class', 'form-control').attr('placeholder', 'Usuário')[0].outerHTML
-	_html.password_field = $(_html.password_field).attr('class', 'form-control').attr('placeholder', 'Senha')[0].outerHTML
-	_html.submit_btn = $(_html.submit_btn).attr('class', 'btn btn-primary btn-block')[0].outerHTML
-
-	$('#TABLE_MID_MPAGE').remove();
-
 	$('#MPW0005IMAGE1').css({
 		padding: '25px'
 	});
 
-	context = {
-		form: {
-			user: _html.user_field,
-			gxstate: _html.gxstate_field,
-			password: _html.password_field,
-			submit_btn: _html.submit_btn,
-			meta: {
-				id: $('#MAINFORM').attr('id'),
-				action: $('#MAINFORM').attr('action'),
-				onsubmit: $('#MAINFORM').attr('onsubmit'),
-				name: $('#MAINFORM').attr('name'),
-				method: $('#MAINFORM').attr('method'),
-			},
-		},
-		help_text: _html.help_text,
-		message: _html.message,
-		gxErrorViewer: _html.gxErrorViewer,
+	els = {
+		user_field: $('#vSIS_USUARIOID'),
+		password_field: $('#vSIS_USUARIOSENHA'),
+		submit_btn: $('input[type="button"][name="BTCONFIRMA"]:first'),
+		gxErrorViewer: $('#gxErrorViewer'),
+		help_text: $('#TEXTBLOCK7')[0].outerHTML,
+		message: $('#TEXTBLOCK3 > div:first')[0].outerHTML,
 	};
 
-	mainform_html = $('#MAINFORM').html();
-	$('#MAINFORM').after(mainform_html);
-	$('#MAINFORM').remove();
+	
+	$('#TABLE_MID_MPAGE').hide();
+
+	context = {
+		help_text: els.help_text,
+		message: els.message,
+		gxErrorViewer: els.gxErrorViewer,
+		fake_user_field: $(els.user_field[0].outerHTML).attr({
+			'id': 'fake_user_field',
+			'name': '',
+			'autofocus': 'autofocus',
+			'placeholder': 'Usuário',
+			'class': 'fake-field form-control'
+		})[0].outerHTML,
+		fake_password_field: $(els.password_field[0].outerHTML).attr({
+			'id': 'fake_password_field',
+			'name': '',
+			'placeholder': 'Senha',
+			'class': 'fake-field form-control'
+		})[0].outerHTML,
+		fake_submit_btn: $(els.submit_btn[0].outerHTML).attr({
+			'id': 'fake_submit_btn',
+			'class': 'btn btn-primary btn-block',
+			'onclick': '',
+			'onfocus': '',
+			'name': ''
+		})[0].outerHTML,
+		fake_gxErrorViewer: $(els.gxErrorViewer[0].outerHTML).attr({
+			'id': 'fake_gxErrorViewer',
+		})[0].outerHTML,
+	};
+
+	var form_invalid = function() {
+		$('.form-group').removeClass('has-success').addClass('has-error');
+		$('#fake_user_field').focus();
+	};
+
+	var form_valid = function() {
+		$('.form-group').removeClass('has-error').removeClass('has-error');
+	};
+
+	// $('#gxErrorViewer').remove();
+	$("#gxErrorViewer").bind("DOMSubtreeModified", function() {
+		var error = $(els.gxErrorViewer)[0].innerHTML;
+		if (error != '') {
+			form_invalid();
+	    	$("#fake_gxErrorViewer").html(error);
+		} else {
+			form_valid();
+		}
+	});
 
 	$('#TABLE_MASTER_MPAGE').after( nunjucks.render('login_form.html', context) );
 
+	// var body_html = $('#MAINFORM').html();
+	// $('#MAINFORM').html( $('div').addClass('wrapper').html(body_html)[0].outerHTML );
+
+	$('#fake_submit_btn').click(function(event){
+		event.preventDefault();
+		$(els.user_field).val( $('#fake_user_field').val() );
+		$(els.password_field).val( $('#fake_password_field').val() );
+		$(els.submit_btn).trigger('click');
+		return false;
+	});
+
+	$('.fake-field').keydown(function(event){
+		if ( event.which == 13 ){
+			event.preventDefault();
+			$('#fake_submit_btn').trigger('click');
+			return false;
+		}
+	});
 });
